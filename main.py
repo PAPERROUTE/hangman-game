c2751db69372422456c3acadbb6334bd82fd50a6
jake <jakegeo@sudomail.com>
2022-05-07 19:15:33 -0700

    step 2

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ word_length = len(chosen_word)
 display = []
 for _ in range(word_length):
     display += "_"
+
+
+#TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    from hangman_art import stages
+    print(stages[lives])
