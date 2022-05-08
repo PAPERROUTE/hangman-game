4aae992404bfaf2232c6e2f1e6942ad4cc8c6dd0
jake <jakegeo@sudomail.com>
2022-05-07 19:18:06 -0700

    step 3

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ print(logo)
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
 
 #TODO-2: - Import the stages from hangman_art.py and make this error go away.
